020d508142e4648176d709e5c7b29143cf0d603e
pakwesi <pakwesiinfornie1@gmail.com>
2020-03-09 18:07:16 +0100

    Database

--- PkCreative/digitalone/settings/base.py
+++ PkCreative/digitalone/settings/base.py
@@ -127,10 +127,22 @@ WSGI_APPLICATION = 'digitalone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
